742aba5e2b2b75ad336d5ba6a9aaf19d5a3977a2
Prashant-Acharya <pacharya97.pa@gmail.com>
2019-05-22 22:10:22 +0545

    Favicon src fixed

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
 		{
 			resolve: 'gatsby-plugin-favicon',
 			options: {
-				logo: './src/static.favicon.png',
+				logo: './src/static/favicon.png',
 				icons: {
 					android: true,
 					appleIcon: true,
