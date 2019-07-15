12c0b3eb978b04201c9d273ce83d72c659581414
Prashant Acharya <prashant.acharya@sagarmatha.edu.np>
2019-07-15 16:52:32 +0545

    Adds manifest & offline plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,7 +39,7 @@ module.exports = {
 					},
 					{
 						resolve: 'gatsby-remark-prismjs',
-						
+
 					}
 				]
 			}
@@ -49,6 +49,21 @@ module.exports = {
 			options: {
 				trackingId: "UA-140813740-1",
 			},
-		}
+		},
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `Big Omega`,
+				short_name: `BigOmega`,
+				start_url: `/`,
+				background_color: `#f16063`,
+				theme_color: `#f16063`,
+				// Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+				// see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+				display: `standalone`,
+				icon: `src/images/icon.png`, // This path is relative to the root of the site.
+			},
+		},
+		`gatsby-plugin-offline`
 	]
 }
